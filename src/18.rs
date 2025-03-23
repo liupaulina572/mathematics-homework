8f8ee2bdf3d029849d9742721d0c1091c61d3c47
liupaulina572 <liupaulina572@gmail.com>
2025-03-23 19:38:47 +0000

    Add 18.rs

---
+++ src/18.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Example Rust code for mathematical operations and variables
+    let x = 5;
+    let y = 3.14;
+
+    // Addition of two numbers
+    let sum = x + y;
+    println!("The result of adding {} and {}", y, x);
+    
+    // Multiplication of two numbers
+    let product = x * y;
+    println!("The result of multiplying {} and {}", x, y);
+}
