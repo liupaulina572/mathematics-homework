4e80043a7d26b007d4dafc992802fc1600727c37
liupaulina572 <liupaulina572@gmail.com>
2025-05-18 20:17:52 +0000

    Add 38.rs

---
+++ src/38.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x > 0 {
+        println!("x is greater than zero.");
+    } else if x == 0 {
+        println!("x is equal to zero.");
+    } else {
+        println!("x is less than zero.");
+    }
+}
